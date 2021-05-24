b4dbe7df5878bad6ef2a8f477730fb3bad3152f4
LemanRus <wlad163163@gmail.com>
2021-05-24 21:21:33 +0400

    nominals subscreens added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import NumericProperty, ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.dropdown import DropDown
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.settings import SettingsWithSidebar
 from kivy.config import Config
@@ -11,7 +13,7 @@ Config.read("radiohelper.ini")
 
 
 class RadioHelperScreenManager(ScreenManager):
-   pass
+    pass
 
 
 class MainScreen(Screen):
@@ -23,7 +25,18 @@ class NominalsScreen(Screen):
 
 
 class ResistorScreen(Screen):
-    pass
+
+    def calculate_resistor(self):
+        band_list = ["first", "second"]
+        resistance = 0
+        tolerance = 0
+        nominal = ("black", "brown", "red", "orange", "yellow", "green",
+                   "blue", "violet", "grey", "white")
+        for band in self.ids:
+            if band in band_list:
+                print(self.ids[band].text)
+    def draw_bands(self):
+        pass
 
 
 class CapacitorScreen(Screen):
@@ -66,11 +79,11 @@ class RadioHelperApp(App):
         self.use_kivy_settings = False
         return RadioHelperScreenManager()
 
-    def build_config(self, config) :
+    def build_config(self, config):
         config.setdefaults("font", {
-            "Размер шрифта заголовков" : 150,
-            "Размер шрифта текста" : 50,
-            })
+            "Размер шрифта заголовков": 150,
+            "Размер шрифта текста": 50,
+        })
 
     def build_settings(self, settings):
         settings.add_json_panel("Panel Name",
