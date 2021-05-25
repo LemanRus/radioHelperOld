730e0323806b6ee14dcba455a9b4fd9c538455e4
LemanRuss <wlad163@gmail.com>
2021-05-25 17:26:36 +0400

    -

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from kivy.uix.dropdown import DropDown
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.settings import SettingsWithSidebar
 from kivy.config import Config
+from kivy.uix.spinner import Spinner
+from kivy.uix.widget import Widget
 
 from settingsjson import settings_json
 
@@ -26,23 +28,82 @@ class NominalsScreen(Screen):
 
 
 class ResistorScreen(Screen):
-    nominal = ["black", "brown", "red", "orange", "yellow", "green",
-               "blue", "violet", "grey", "white"]
+    nominal = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4, "green": 5,
+               "blue": 6, "violet": 7, "grey": 8, "white": 9}
+    multiplier = {"gold": 0.1, "silver": 0.01, "black": 1, "brown": 10, "red": 100, "orange": 1000, "yellow": 10000,
+                  "green": 100000, "blue": 1000000, "violet": 10000000, "grey": 100000000}
+    tolerance = {"gold": "±5%", "silver": "±10%", "black": "±0,005%", "brown": "±1%", "red": "±2%", "orange": "±0,01%",
+                 "yellow": "±0,02%", "green": "±0,5%", "blue": "±0,25%", "violet": "±0,1%", "grey": "±0,05%"}
 
-    def calculate_resistor(self):
-        band_list = ["first", "second"]
-        resistance = 0
-        tolerance = 0
-
-        for band in self.ids:
-            if band in band_list:
-                print(self.ids[band].text)
+    def calculate_resistor(self, value):
+        if value == "4":
+            self.calculate_four_bands_resistor()
+        elif value == "5":
+            self.calculate_five_bands_resistor()
 
     def build_resistor(self, value):
-        value = int(value)
+        if value == "4":
+            self.build_four_bands_resistor()
+        elif value == "5":
+            self.build_five_bands_resistor()
+
+    def build_four_bands_resistor(self):
         self.ids.resistor_bands.clear_widgets()
-        for i in range(0, value):
-            self.ids["resistor_bands"].add_widget(Button(text=str(i)))
+
+        self.ids["resistor_bands"].add_widget(Spinner(
+            text=list(self.nominal.keys())[1], values=list(self.nominal.keys())[1:],
+        ))
+        self.ids["resistor_bands"].add_widget(Widget(size_hint_x=0.2))
+
+        self.ids["resistor_bands"].add_widget(Spinner(
+            text=list(self.nominal.keys())[0], values=self.nominal.keys()))
+        self.ids["resistor_bands"].add_widget(Widget(size_hint_x=0.2))
+
+        self.ids["resistor_bands"].add_widget(Spinner(
+            text=list(self.multiplier.keys())[0], values=self.multiplier.keys()))
+        self.ids["resistor_bands"].add_widget(Widget(size_hint_x=0.2))
+
+        self.ids["resistor_bands"].add_widget(Spinner(
+            text=list(self.tolerance.keys())[0], values=self.tolerance.keys()))
+
+    def build_five_bands_resistor(self):
+        self.ids.resistor_bands.clear_widgets()
+
+        self.ids["resistor_bands"].add_widget(Spinner(
+            text=list(self.nominal.keys())[1], values=list(self.nominal.keys())[1:],
+        ))
+        self.ids["resistor_bands"].add_widget(Widget(size_hint_x=0.2))
+
+        self.ids["resistor_bands"].add_widget(Spinner(
+            text=list(self.nominal.keys())[0], values=self.nominal.keys()))
+        self.ids["resistor_bands"].add_widget(Widget(size_hint_x=0.2))
+
+        self.ids["resistor_bands"].add_widget(Spinner(
+            text=list(self.nominal.keys())[0], values=self.nominal.keys()))
+        self.ids["resistor_bands"].add_widget(Widget(size_hint_x=0.2))
+
+        self.ids["resistor_bands"].add_widget(Spinner(
+            text=list(self.multiplier.keys())[0], values=self.multiplier.keys()))
+        self.ids["resistor_bands"].add_widget(Widget(size_hint_x=0.2))
+
+        self.ids["resistor_bands"].add_widget(Spinner(
+            text=list(self.tolerance.keys())[0], values=self.tolerance.keys()))
+
+    def calculate_four_bands_resistor(self):
+        resistance = (self.nominal[self.ids.resistor_bands.children[6].text] * 10 +
+                      self.nominal[self.ids.resistor_bands.children[4].text]) * \
+                      self.multiplier[self.ids.resistor_bands.children[2].text]
+
+        tolerance = self.tolerance[self.ids.resistor_bands.children[0].text]
+        if resistance < 1000:
+            self.ids.resistance.text = "{} {}".format(resistance, tolerance)
+        elif resistance < 1000000:
+            self.ids.resistance.text = "{:.0f} кОм {}".format(resistance / 1000, tolerance)
+        else:
+            self.ids.resistance.text = "{:.0f} МОм {}".format(resistance / 1000000, tolerance)
+
+    def calculate_five_bands_resistor(self):
+        pass
 
 
 class CapacitorScreen(Screen):
