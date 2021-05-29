c881bd43094f545ed0ae4d560f5e088c8fbafa56
LemanRus <wlad163163@gmail.com>
2021-05-29 23:06:57 +0400

    Добавлен расчёт SMD резисторов

--- main.py
+++ main.py
@@ -43,6 +43,19 @@ class ResistorScreen(Screen):
               "brown": [0.4, 0.22, 0, 1], "red": [1, 0, 0, 1], "orange": [0.98, 0.45, 0.02, 1],
               "yellow": [1, 1, 0, 1], "green": [0.05, 0.64, 0.05, 1], "blue": [0.05, 0.54, 0.95, 1],
               "violet": [0.54, 0.14, 0.59, 1], "grey": [0.5, 0.5, 0.5, 1], "white": [1, 1, 1, 1]}
+    eia96 = {"01": 100, "02": 102, "03": 105, "04": 107, "05": 110, "06": 113, "07": 115, "08": 118, "09": 121,
+             "10": 124, "11": 127, "12": 130, "13": 133, "14": 137, "15": 140, "16": 143, "17": 147, "18": 150,
+             "19": 154, "20": 158, "21": 162, "22": 165, "23": 169, "24": 174, "25": 178, "26": 182, "27": 187,
+             "28": 191, "29": 196, "30": 200, "31": 205, "32": 210, "33": 215, "34": 221, "35": 226, "36": 232,
+             "37": 237, "38": 243, "39": 249, "40": 255, "41": 261, "42": 267, "43": 274, "44": 280, "45": 287,
+             "46": 294, "47": 301, "48": 309, "49": 316, "50": 324, "51": 332, "52": 340, "53": 348, "54": 357,
+             "55": 365, "56": 374, "57": 383, "58": 392, "59": 402, "60": 412, "61": 422, "62": 432, "63": 442,
+             "64": 453, "65": 464, "66": 475, "67": 487, "68": 499, "69": 511, "70": 523, "71": 536, "72": 549,
+             "73": 562, "74": 576, "75": 590, "76": 604, "77": 619, "78": 634, "79": 649, "80": 665, "81": 681,
+             "82": 698, "83": 715, "84": 732, "85": 750, "86": 768, "87": 787, "88": 806, "89": 825, "90": 845,
+             "91": 866, "92": 887, "93": 909, "94": 931, "95": 953, "96": 976}
+    eia96_multiplier = {"Z": 0.001, "Y": 0.01, "R": 0.01, "X": 0.1, "S": 0.1, "A": 1, "B": 10, "H": 10, "C": 100,
+                        "D": 1000, "E": 10000, "F": 100000}
 
     def select_color(self):
         pass
@@ -117,7 +130,44 @@ class ResistorScreen(Screen):
                     band.color = [1, 1, 1, 1]
 
     def calculate_smd_resistor(self, marking):
-        print(marking)
+        self.ids.smd_resistance.text = ""
+        multiplier = "not assigned"
+        markings = "empty"
+        resistance = ""
+
+        if marking in ["0", "00", "000", "0000"]:
+            resistance = 0
+        elif "R" in marking and marking[2] != "R":
+            if (len(marking) == 3 or len(marking)) == 4:
+                markings = marking.split("R")
+                resistance = float("{}.{}".format(markings[0], markings[1]))
+            else:
+                self.ids.smd_resistance.text = "Неверный ввод"
+        elif "r" in marking:
+            if (len(marking) == 3 or len(marking)) == 4:
+                markings = marking.split("r")
+                resistance = float("{}.{}".format(markings[0], markings[1]))
+            else:
+                self.ids.smd_resistance.text = "Неверный ввод"
+        elif len(marking) == 3:
+            if marking[2].isalpha() and marking[2] in self.eia96_multiplier.keys():
+                multiplier = self.eia96_multiplier[marking[2]]
+                resistance = self.eia96[marking[:2]] * multiplier
+            else:
+                resistance = float(marking[:2]) * 10 ** (float(marking[2]))
+        elif len(marking) == 4:
+            resistance = float(marking[:3]) * 10 ** (float(marking[2]))
+        else:
+            self.ids.smd_resistance.text = "Неверный ввод"
+
+        if resistance:
+            if resistance < 1000:
+                self.ids.smd_resistance.text = "{:g} Ом".format(resistance)
+            elif resistance < 1000000:
+                self.ids.smd_resistance.text = "{:g} кОм".format(resistance / 1000)
+            else:
+                self.ids.smd_resistance.text = "{:g} МОм".format(resistance / 1000000)
+
 
 class CapacitorScreen(Screen):
     pass
