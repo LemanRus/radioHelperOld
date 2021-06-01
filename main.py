f231dedf40163c18d3be6430206f58a2eaef750a
LemanRuss <wlad163@gmail.com>
2021-06-01 16:37:29 +0400

    Скелет для распиновок

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.settings import SettingsWithSidebar
+from kivy.uix.settings import SettingsWithSidebar, SettingsWithNoMenu, SettingsWithSpinner, SettingsWithTabbedPanel
 from kivy.config import Config
 from kivy.uix.spinner import Spinner
 from kivy.uix.widget import Widget
@@ -60,9 +60,12 @@ class AboutScreen(Screen):
 
 class RadioHelperApp(App):
 
+    app_font_size = NumericProperty()
+
     def build(self):
         self.settings_cls = SettingsWithSidebar
         self.config.items("font")
+        self.app_font_size = self.config.getint("font", "header_size")
         self.use_kivy_settings = False
         return RadioHelperScreenManager()
 
@@ -73,18 +76,14 @@ class RadioHelperApp(App):
         })
 
     def build_settings(self, settings):
-        settings.add_json_panel("Panel Name",
+        settings.add_json_panel("Размеры текста",
                                 self.config,
                                 data=settings_json)
 
     def on_config_change(self, config, section, key, value):
-        if key == "header_size":
-            self.root.ids.one.ids.title1.font_size = value
-        if key == "text_size":
-            for up_id in self.root.ids:
-                if up_id != 'title1':
-                    for low_id in self.root.ids[up_id].ids:
-                        self.root.ids[up_id].ids[low_id].font_size = value
+        config.set(section=section, option=key, value=value)
+        config.write()
+        self.app_font_size = self.config.getint("font", "header_size")
 
 
 if __name__ == '__main__':
