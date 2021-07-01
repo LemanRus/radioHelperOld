4a9fb119defd9aa9bfb49d4c1e172f36c7108f8b
LemanRuss <wlad163@gmail.com>
2021-07-01 17:12:02 +0400

    Добавлены экраны делителя напряжения (нужны правки)

--- VoltageDividerCalculateScreen.py
+++ VoltageDividerCalculateScreen.py
@@ -9,17 +9,10 @@ class VoltageDividerCalculateScreen(Screen):
 
     dynamic_vars = DictProperty({})
 
-    def build_divider(self, choose):
-        """self.ids.divider_box.clear_widgets()
-        if choose == "Расчёт выходного напряжения":
-            self.ids.divider_box.add_widget(Label(text="TESTTESTTESTTEST"))
-        elif choose == "Расчёт сопротивления":
-            print("EGGS")"""
 
-        if choose == "Расчёт выходного напряжения":
-            Builder.load_file("kv/test.kv")
-        else:
-            self.ids.divider_box.clear_widgets()
+class DividerVoltageScreen(Screen):
+    pass
 
-class DividerBox(BoxLayout):
+
+class DividerResistanceScreen(Screen):
     pass
