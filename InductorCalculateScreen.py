b7ad8de5abc76f6a3a57ee1049a2439309e79a41
LemanRuss <wlad163163@gmail.com>
2021-09-01 23:16:30 +0400

    Испавлен расчёт индуктивности

--- InductorCalculateScreen.py
+++ InductorCalculateScreen.py
@@ -8,17 +8,16 @@ class InductorCalculateScreen(Screen):
     def inductor_calculate_turns(self, henrys, diameter, oneturn):
         try:
             henrys = float(henrys)
-            diameter = float(diameter)                                 # в формуле сантиметры, во вводе миллиметры
-            oneturn = float(oneturn)
-            inductor_length = (50 * oneturn ** 2 * henrys + math.sqrt(5) *
-                               math.sqrt(500 * oneturn ** 4 * henrys ** 2 +
-                                         9 * oneturn ** 2 * diameter ** 3 * henrys)) / diameter ** 2
+            diameter = float(diameter) / 10                                 # в формуле сантиметры, во вводе миллиметры
+            oneturn = float(oneturn) / 10
+            inductor_length = (50 * oneturn ** 2 * henrys + math.sqrt(5) * math.sqrt(500 * oneturn ** 4 * henrys ** 2 +
+                               9 * oneturn ** 2 * diameter ** 3 * henrys)) / diameter ** 2
 
             inductor_turns = inductor_length / oneturn
             inductor_turns_int = round(inductor_turns, 0)
-            inductor_length_int = inductor_turns_int * oneturn
+            inductor_length_int = inductor_turns_int * oneturn * 10
 
-            self.ids.inductor_length.text = "{:g} мм".format(inductor_length)
+            self.ids.inductor_length.text = "{:g} мм".format(inductor_length * 10)
             self.ids.inductor_length_int.text = "{:g} мм".format(inductor_length_int)
             self.ids.inductor_turns.text = "{:g} витка(ов)".format(inductor_turns)
             self.ids.inductor_turns_int.text = "{:g} витка(ов)".format(inductor_turns_int)
@@ -44,6 +43,5 @@ class InductorCalculateScreen(Screen):
                                                                                                    formfactor) ** 1.44))
             self.ids.induction.text = "{:g} мкГн".format(induction)
         except Exception:
-            print(Exception)
             self.ids.induction.text = "Неверный ввод!"
 
