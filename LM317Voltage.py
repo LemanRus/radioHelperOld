5a3ee2bc1fa86a18ef10eebb68452ba3c917ea9e
LemanRuss <wlad163@gmail.com>
2021-09-07 16:08:03 +0400

    Версия приложения 0.1.2

--- LM317Voltage.py
+++ LM317Voltage.py
@@ -1,6 +1,63 @@
 from kivy.uix.screenmanager import Screen
+from StandardRows import StandardRows
 
 
 class LM317Voltage(Screen):
-    def calulate_lm317_voltage(self):
-        pass
+
+    def calculate_lm317_voltage(self, vout, r1, iout, vin):
+        try:
+            vout = float(vout)
+            r1 = float(r1)
+            iout = float(iout)
+            vin = float(vin)
+            if iout > 5:
+                self.ids.lm317_r2_output.text = "Ток нагрузки должен быть меньше 5А!"
+                self.ids.lm317_r2_corrected_output.text = "Ток нагрузки должен быть меньше 5А!"
+                self.ids.lm317_r2_output.text = ""
+                self.ids.lm317_vout_output.text = ""
+                self.ids.lm317_recommend_output.text = ""
+                self.ids.lm317_power_output.text = ""
+            else:
+                r2 = r1 * (vout / 1.25 - 1)
+                if r2 == 0:
+                    result = "0 Ом (перемычка)"
+                elif r2 < 1000:
+                    result = "{:g} Ом".format(r2)
+                elif r2 < 1000000:
+                    result = "{:g} кОм".format(r2 / 1000)
+                else:
+                    result = "{:g} МОм".format(r2 / 1000000)
+                r2_corrected = StandardRows.calculate_standard_resistor(r2, False)
+
+                power = (vin - vout) * iout
+
+                vout_corrected = 1.25 * (1 + r2_corrected / r1)
+
+                if iout > 3:
+                    recommend = "LM338"
+                elif iout > 1.5:
+                    recommend = "LM350"
+                else:
+                    recommend = "LM317"
+
+                if r2_corrected == 0:
+                    result_corrected = "0 Ом (перемычка)"
+                elif r2_corrected < 1000:
+                    result_corrected = "{:g} Ом".format(r2_corrected)
+                elif r2_corrected < 1000000:
+                    result_corrected = "{:g} кОм".format(r2_corrected / 1000)
+                else:
+                    result_corrected = "{:g} МОм".format(r2_corrected / 1000000)
+
+                self.ids.lm317_r2_corrected_output.text = result_corrected
+                self.ids.lm317_r2_output.text = result
+                self.ids.lm317_vout_output.text = "{:g} В".format(vout_corrected)
+                self.ids.lm317_recommend_output.text = recommend
+                self.ids.lm317_power_output.text = "{:g} Вт".format(power)
+        except Exception:
+            self.ids.lm317_r2_output.text = "Неверный ввод!"
+            self.ids.lm317_r2_corrected_output.text = "Неверный ввод!"
+            self.ids.lm317_r2_output.text = "Неверный ввод!"
+            self.ids.lm317_vout_output.text = "Неверный ввод!"
+            self.ids.lm317_recommend_output.text = "Неверный ввод!"
+            self.ids.lm317_power_output.text = "Неверный ввод!"
