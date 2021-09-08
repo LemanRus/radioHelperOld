3dbf08cdf7b4e67849790b56d3fa57894bf55f30
LemanRuss <wlad163163@gmail.com>
2021-09-08 22:01:58 +0400

    Краш системы на Минт

--- LM317Current.py
+++ LM317Current.py
@@ -9,10 +9,6 @@ class LM317Current(Screen):
 
             r1 = 1.25 / iout
 
-            r1_corrected = StandardRows.calculate_standard_resistor(r1, False)
-
-            iout_corrected = 1.25 / r1_corrected
-
             if iout > 3:
                 recommend = "LM338"
             elif iout > 1.5:
@@ -20,16 +16,16 @@ class LM317Current(Screen):
             else:
                 recommend = "LM317"
 
-            if r1_corrected == 0:
+            if r1 == 0:
                 result_corrected = "0 Ом (перемычка)"
-            elif r1_corrected < 1000:
-                result_corrected = "{:g} Ом".format(r1_corrected)
-            elif r1_corrected < 1000000:
-                result_corrected = "{:g} кОм".format(r1_corrected / 1000)
+            elif r1 < 1000:
+                result_corrected = "{:g} Ом".format(r1)
+            elif r1 < 1000000:
+                result_corrected = "{:g} кОм".format(r1 / 1000)
             else:
-                result_corrected = "{:g} МОм".format(r1_corrected / 1000000)
+                result_corrected = "{:g} МОм".format(r1 / 1000000)
 
-            power_r1 = iout_corrected ** 2 * r1_corrected
+            power_r1 = r1 ** 2 * r1
             if vout:
                 vout = float(vout)
                 vin_corrected = vout + 3.7
@@ -39,7 +35,7 @@ class LM317Current(Screen):
 
             self.ids.lm317_r1_output_cur.text = result_corrected
             self.ids.lm317_r1_power_output_cur.text = "{:g} Вт".format(power_r1)
-            self.ids.lm317_iout_output_cur.text = "{:g} А".format(iout_corrected)
+
             self.ids.lm317_recommend_output_cur.text = recommend
 
 
