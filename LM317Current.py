bbf13d8297c457ee8aae1a4501196f6b4c0fc5f5
LemanRuss <wlad163@gmail.com>
2021-09-09 16:53:50 +0400

    Уточнён расчёт LM317

--- LM317Current.py
+++ LM317Current.py
@@ -6,45 +6,69 @@ class LM317Current(Screen):
     def calculate_lm317_current(self, iout, vout):
         try:
             iout = float(iout)
+            if iout <= 5:
+                r1 = 1.25 / iout
+                
+                r1_corrected = StandardRows.calculate_standard_resistor(r1, True)
+                
+                if iout > 3:
+                    recommend = "LM338"
+                elif iout > 1.5:
+                    recommend = "LM350"
+                else:
+                    recommend = "LM317"
+    
+                if r1 < 1000:
+                    result = "{:g} Ом".format(r1)
+                elif r1 < 1000000:
+                    result = "{:g} кОм".format(r1 / 1000)
+                else:
+                    result = "{:g} МОм".format(r1 / 1000000)
 
-            r1 = 1.25 / iout
+                if r1_corrected < 1000:
+                    result_corrected = "{:g} Ом".format(r1_corrected)
+                elif r1_corrected < 1000000:
+                    result_corrected = "{:g} кОм".format(r1_corrected / 1000)
+                else:
+                    result_corrected = "{:g} МОм".format(r1_corrected / 1000000)
 
-            if iout > 3:
-                recommend = "LM338"
-            elif iout > 1.5:
-                recommend = "LM350"
+                iout_corrected = 1.25 / r1_corrected
+
+                power_r1 = iout ** 2 * r1
+                power_corrected = iout_corrected ** 2 * r1_corrected
+
+                if vout:
+                    vout = float(vout)
+                    if not (3 <= vout <= 38):
+                        self.ids.lm317_vin_output_cur.text = "Падение напряжения должно быть больше 2В и меньше 38В!"
+                    else:
+                        vin_corrected = vout + 3.7
+                        self.ids.lm317_vin_output_cur.text = "{:g} А".format(vin_corrected)
+                else:
+                    self.ids.lm317_vin_output_cur.text = ""
+    
+                self.ids.lm317_r1_output_cur.text = result
+                self.ids.lm317_r1_corrected_output_cur.text = result_corrected
+                self.ids.lm317_r1_power_output_cur.text = "{:g} Вт".format(power_r1)
+                self.ids.lm317_r1_power_corrected_output_cur.text = "{:g} Вт".format(power_corrected)
+                self.ids.lm317_iout_corrected_output_cur.text = "{:g} А".format(iout_corrected)
+                self.ids.lm317_recommend_output_cur.text = recommend
             else:
-                recommend = "LM317"
-
-            if r1 == 0:
-                result_corrected = "0 Ом (перемычка)"
-            elif r1 < 1000:
-                result_corrected = "{:g} Ом".format(r1)
-            elif r1 < 1000000:
-                result_corrected = "{:g} кОм".format(r1 / 1000)
-            else:
-                result_corrected = "{:g} МОм".format(r1 / 1000000)
-
-            power_r1 = r1 ** 2 * r1
-            if vout:
-                vout = float(vout)
-                vin_corrected = vout + 3.7
-                self.ids.lm317_vin_output_cur.text = "{:g} А".format(vin_corrected)
-            else:
+                self.ids.lm317_r1_output_cur.text = "Ток должен быть менее 5А!"
+                self.ids.lm317_r1_corrected_output_cur.text = ""
+                self.ids.lm317_r1_power_output_cur.text = ""
+                self.ids.lm317_r1_power_corrected_output_cur.text = ""
+                self.ids.lm317_iout_corrected_output_cur.text = ""
+                self.ids.lm317_recommend_output_cur.text = ""
                 self.ids.lm317_vin_output_cur.text = ""
-
-            self.ids.lm317_r1_output_cur.text = result_corrected
-            self.ids.lm317_r1_power_output_cur.text = "{:g} Вт".format(power_r1)
-
-            self.ids.lm317_recommend_output_cur.text = recommend
-
 
 
         except Exception:
             self.ids.lm317_r1_output_cur.text = "Неверный ввод!"
+            self.ids.lm317_r1_corrected_output_cur.text = "Неверный ввод!"
             self.ids.lm317_r1_power_output_cur.text = "Неверный ввод!"
-            self.ids.lm317_iout_output_cur.text = "Неверный ввод!"
+            self.ids.lm317_r1_power_corrected_output_cur.text = "Неверный ввод!"
+            self.ids.lm317_iout_corrected_output_cur.text = "Неверный ввод!"
             self.ids.lm317_recommend_output_cur.text = "Неверный ввод!"
             self.ids.lm317_vin_output_cur.text = "Неверный ввод!"
-
 
